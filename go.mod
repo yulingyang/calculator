c365f019a62695da53caf8e954019dc230a61abe
yyl <1882@qq.com>
2022-01-07 16:07:02 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yulingyang/calculator
+
+go 1.16
---
+++ sum.go
@@ -0,0 +1,13 @@
+package calculator
+
+var logMessage = "[LOG]"
+
+var Version = "1.0"
+
+func internalSum(sum int) int{
+	return sum -1
+}
+
+func Sum(a,b int) int {
+	return a+b
+}
